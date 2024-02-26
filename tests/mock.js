deb0d65d7f92fc8ed7ecfc9cb33b298b66b28168
Thomas Sloboda <thomas@sloboda.fr>
2024-02-26 23:50:00 +0100

    feat(filter): add filter feature (#4)

---
+++ tests/mock.js
@@ -0,0 +1,45 @@
+const countriesWithAnimalsContainingRY = [
+  {
+    name: "USA",
+    people: [
+      {
+        name: "John",
+        animals: [
+          { name: "Daija Romaguery" },
+          { name: "Wilburn Little" },
+          { name: "Floy Stiedemann" },
+          { name: "Margie Pfeffer" },
+        ],
+      },
+      {
+        name: "Jane",
+        animals: [
+          { name: "Xander White" },
+          { name: "Lydia Mitchell" },
+          { name: "Mitchell Towne" },
+          { name: "Chloe Miller" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Canada",
+    people: [
+      {
+        name: "Pamela",
+        animals: [{ name: "Bryon Kuhn" }, { name: "Rylee Kuhn" }],
+      },
+    ],
+  },
+  {
+    name: "Mexico",
+    people: [
+      {
+        name: "Luis",
+        animals: [{ name: "Oswaldo Kozey" }, { name: "Bud Will" }],
+      },
+    ],
+  },
+];
+
+module.exports = { countriesWithAnimalsContainingRY };
